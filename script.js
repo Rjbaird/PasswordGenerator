1afc3ad1517ec80105f2eebaa135788eb99de044
Ryan Baird <rjbaird09@gmail.com>
2020-06-11 15:43:20 -0500

    initial commit with temp css

---
+++ script.js
@@ -0,0 +1,20 @@
+// function generatePassword (params) {
+    // Ask how many character the password should be
+    var passwordLength = prompt("How many characters should the password be?" + "\n" + "We recommend at least 10");
+    // Ask if the user wants numbers in the password
+    var passwordNumbers = confirm("Do you want numbers in the password?" + "\n" + "Press Ok for yes and Cancel for no");
+    // Ask if the user wants special characters 
+    var passwordSpecial = confirm("Do you want special characters in the password?" + "\n" + "Press Ok for yes and Cancel for no");
+    // Ask if the user wants lower case letters
+    var passwordLower = confirm("Do you want lower case letters in the password?" + "\n" + "Press Ok for yes and Cancel for no");
+    // Ask if the user wants upper case letters
+    var passwordUpper = confirm("Do you want UPPER CASE letters in the password?" + "\n" + "Press Ok for yes and Cancel for no");
+    
+    
+    
+    
+    // // Paste code into textbox
+    // var completePassword 
+    // // Paste code to the clipboard
+    // var pastedPassword = document.execCommand("paste")
+// }
