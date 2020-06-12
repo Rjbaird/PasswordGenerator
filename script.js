5e1bec24ab67416fde63ab9a8198fdd60a412fb0
Ryan Baird <rjbaird09@gmail.com>
2020-06-11 22:41:47 -0500

    Updated html, css and js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-function generatePassword (params) {
-    // Ask how many character the password should be
-    var passwordLength = prompt("How many characters should the password be?" + "\n" + "We recommend at least 10");
-    // Ask if the user wants numbers in the password
-    var passwordNumbers = confirm("Do you want numbers in the password?" + "\n" + "Press Ok for yes and Cancel for no");
-    // Ask if the user wants special characters 
-    var passwordSpecial = confirm("Do you want special characters in the password?" + "\n" + "Press Ok for yes and Cancel for no");
-    // Ask if the user wants lower case letters
-    var passwordLower = confirm("Do you want lower case letters in the password?" + "\n" + "Press Ok for yes and Cancel for no");
-    // Ask if the user wants upper case letters
-    var passwordUpper = confirm("Do you want UPPER CASE letters in the password?" + "\n" + "Press Ok for yes and Cancel for no");
-    
-    
-    
-    
-    // // Paste code into textbox
-    // var completePassword 
-    // // Paste code to the clipboard
-    // var pastedPassword = document.execCommand("paste")
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var special = "\"!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
